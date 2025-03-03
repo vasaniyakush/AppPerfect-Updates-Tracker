5ea73bb6eb6ca4bd5b079865d83c6aad7c3ac3d1
Kush Vasaniya <vasaniyakush@gmail.com>
2025-03-03 18:24:09 +0530

    Update constants.ts
    updated category list

--- component/constants.ts
+++ component/constants.ts
@@ -1,1 +1,1 @@
-export const category_list: string[] = ["Plotly", "Data Pipelines", "Navigator 2.0 Backend: Reporting Service", "Compute Reporting", "Navigator 1.0"];
+export const category_list: string[] = ["Plotly", "Data Pipelines", "Navigator 1.0 Reporting", "Compute Reporting", "Navigator 0.1 Reporting", "General"];
